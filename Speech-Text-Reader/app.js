e0c956a54d4c8b571e08ec795f5d5644147c7f66
Sahil Gupta <seekersahil@gmail.com>
2023-01-07 22:09:47 +0530

    finished speech text reader project

--- Speech-Text-Reader/app.js
+++ Speech-Text-Reader/app.js
@@ -3,7 +3,7 @@ const voiceSelect = document.getElementById("voices");
 const textArea = document.getElementById("text");
 const readBtn = document.getElementById("read");
 const toggleBtn = document.getElementById("toggle");
-const bloseBtn = document.getElementById("close");
+const closeBtn = document.getElementById("close");
 
 const data = [
   {
@@ -55,3 +55,77 @@ const data = [
     text: "I Want To Go To Grandmas",
   },
 ];
+
+data.forEach(createBox);
+
+//create speech boxes
+function createBox(item) {
+  const box = document.createElement("div");
+
+  const { image, text } = item;
+
+  box.classList.add("box");
+  box.innerHTML = `
+    <img src='${image}' alt='${text}'>
+    <p class='info'>${text}</p>
+  `;
+  box.addEventListener("click", () => {
+    setTextMessage(text);
+    speakText();
+    box.classList.add("active");
+    setTimeout(() => box.classList.remove("active"), 1000);
+  });
+  main.appendChild(box);
+}
+
+//init speech synthesis
+const message = new SpeechSynthesisUtterance();
+
+//set text
+function setTextMessage(text) {
+  message.text = text;
+}
+
+//speaking the text
+function speakText() {
+  speechSynthesis.speak(message);
+}
+
+//store voices
+let voices = [];
+function populateVoices() {
+  console.log(speechSynthesis);
+  voices = speechSynthesis.getVoices();
+  console.log(voices);
+  voices.forEach((voice) => {
+    const option = document.createElement("option");
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+    voiceSelect.appendChild(option);
+  });
+}
+
+//Voices changed
+speechSynthesis.addEventListener("voiceschanged", populateVoices);
+
+//text box show and hide
+toggleBtn.addEventListener("click", () => {
+  document.getElementById("text-box").classList.toggle("show");
+});
+
+closeBtn.addEventListener("click", () => {
+  document.getElementById("text-box").classList.remove("show");
+});
+
+//change voice
+voiceSelect.addEventListener("change", (e) => {
+  message.voice = voices.find((v) => v.name === e.target.value);
+});
+
+//Read text button
+readBtn.addEventListener("click", () => {
+  setTextMessage(textArea.value);
+  speakText();
+});
+
+populateVoices();
